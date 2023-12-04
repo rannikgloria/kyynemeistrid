35a61565507bc43b2d60acc702c8c79dee31dc8f
rannikgloria <144918658+rannikgloria@users.noreply.github.com>
2023-12-04 20:58:26 +0200

    kommentaarid, viide

--- html_files/tagasiside.js
+++ html_files/tagasiside.js
@@ -1,31 +1,39 @@
 var currentSlide = 1;
 
-
+//näitab antud indeksiga slaidi
 function showSlide(slideIndex) {
+  //annab järjendi klassi elementidega
   const slides = document.getElementsByClassName('carouselImgs');
+  //kui indeks liiga suur, alustab otsast peale
   if (slideIndex > slides.length) { currentSlide = 1 }
+  //kui indeks liiga väike, läheb lõppu
   if (slideIndex < 1) { currentSlide = slides.length }
+  //määrab vaate stiili
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
   slides[currentSlide - 1].style.display = 'flex'
 }
 
-
 function nextSlide() {
   showSlide(currentSlide += 1);
 }
 
-function previousSlide() {
+function previousSlide() { 
   showSlide(currentSlide -= 1);
 }
 
+//akna laadimisel käivitab funktsiooni
 window.onload = function () {
   showSlide(currentSlide);
+  //eelmine slaid, kui vajutatakse tagasi nuppu
   document.getElementById('prev').addEventListener('click', function () {
     previousSlide();
   })
+  //järgmine slaid, kui vajutatakse edasi nuppu
   document.getElementById('next').addEventListener('click', function () {
     nextSlide();
   })
 }
+
+// viide: https://dev.to/saravanakumarputta/js-snippets-carousel-slider-using-plain-html-css-and-js-5fjc
